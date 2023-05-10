d73f7f3a5c97b1d76e593fd2985af03a0d5487b1
abdicalool <cabdirisaq22@gmail.com>
2023-05-11 00:23:19 +0200

    initial commit

--- src/main/java/com/javaboots/javaboots/JavabootsApplication.java
+++ src/main/java/com/javaboots/javaboots/JavabootsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavabootsApplication {
-
+//hej
 	public static void main(String[] args) {
 		SpringApplication.run(JavabootsApplication.class, args);
 	}
--- src/test/java/com/javaboots/javaboots/JavabootsApplicationTests.java
+++ src/test/java/com/javaboots/javaboots/JavabootsApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JavabootsApplicationTests {
-
+//hej abdui
 	@Test
 	void contextLoads() {
 	}
